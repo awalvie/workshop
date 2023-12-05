4de0e3bbbebc7684bcddb5ec6a909a06f4cc886e
awalvie <agrawal.vishesh.178@gmail.com>
2023-12-05 15:38:47 +0100

    aoc: part solution, day two - part one

---
+++ aoc/2023/day-2/main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"bufio"
+	"log/slog"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Cube struct {
+	Count int
+	Color string
+}
+
+type Result struct {
+	Cubes []Cube
+}
+
+type Set struct {
+	Result []Result
+}
+
+type Game struct {
+	ID   int
+	Sets []Set
+}
+
+// ParseInput parses the input file and returns a slice of strings
+func ParseInput() ([]Game, error) {
+	var games []Game
+
+	// Open input.txt for reading
+	f, err := os.Open("input.txt")
+	if err != nil {
+		slog.Error("Error opening input.txt: " + err.Error())
+	}
+	defer f.Close()
+
+	// Read the file line by line
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		// Ex: Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+		var game Game
+
+		// Split the line on the colon
+		sLine := strings.Split(line, ":")
+
+		// Get the game ID
+		ID, err := strconv.Atoi((strings.Split(sLine[0], " ")[1]))
+		if err != nil {
+			slog.Error("Error converting string to int: " + err.Error())
+		}
+
+		// Set the game ID
+		game.ID = ID
+
+		// Split sLine on the semicolon
+		sSets := strings.Split(sLine[1], ";")
+
+		// Loop through the sets
+		for _, sSet := range sSets {
+			var set Set
+
+			// Trim the whitespace
+			sSet = strings.TrimSpace(sSet)
+
+			// Split the set on the comma
+			sResults := strings.Split(sSet, ",")
+
+			// Loop through the results
+			for _, sResult := range sResults {
+				var result Result
+
+				// Trim the whitespace
+				sResult = strings.TrimSpace(sResult)
+
+				// Split the result on the space
+				sCubes := strings.Split(sResult, " ")
+
+				// Get the count
+				count, err := strconv.Atoi(sCubes[0])
+				if err != nil {
+					slog.Error("Error converting string to int: " + err.Error())
+				}
+
+				// Get the color
+				color := sCubes[1]
+
+				// Set the result
+				result.Cubes = append(result.Cubes, Cube{Count: count, Color: color})
+
+				// Append the result to the set
+				set.Result = append(set.Result, result)
+			}
+
+			// Append the set to the game
+			game.Sets = append(game.Sets, set)
+		}
+
+		// Append the game to the games
+		games = append(games, game)
+	}
+
+	return games, nil
+}
+
+// One runs the first part of the challenge
+func One() {
+	_, err := ParseInput()
+	if err != nil {
+		slog.Error("Error parsing input: " + err.Error())
+	}
+}
+
+func main() {
+	// Run the first part of the challenge
+	One()
+}
