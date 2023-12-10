5eb43f7dcfba34a3c600284a831413d1042d0d61
awalvie <agrawal.vishesh.178@gmail.com>
2023-12-10 19:50:04 +0100

    aoc: solution day two

--- aoc/2023/day-2/main.go
+++ aoc/2023/day-2/main.go
@@ -109,13 +109,96 @@ func ParseInput() ([]Game, error) {
 
 // One runs the first part of the challenge
 func One() {
-	_, err := ParseInput()
+	games, err := ParseInput()
 	if err != nil {
 		slog.Error("Error parsing input: " + err.Error())
 	}
+
+	invalidSum := 0
+	gameSum := 0
+
+	// Loop through the games
+	for _, game := range games {
+		gameSum += game.ID
+		found := false
+		// Loop through the sets
+		for _, set := range game.Sets {
+			if !found {
+				// Loop through the results
+				for _, result := range set.Result {
+					// Loop through the cubes
+					blueCount := 0
+					redCount := 0
+					greenCount := 0
+
+					for _, cube := range result.Cubes {
+						// Add the count to the color
+						if cube.Color == "blue" {
+							blueCount += cube.Count
+						} else if cube.Color == "red" {
+							redCount += cube.Count
+						} else if cube.Color == "green" {
+							greenCount += cube.Count
+						}
+					}
+
+					if redCount > 12 || blueCount > 14 || greenCount > 13 {
+						invalidSum += game.ID
+						found = true
+						break
+					}
+				}
+			}
+		}
+	}
+
+	slog.Info("Solution One: " + strconv.Itoa(gameSum-invalidSum))
+}
+
+// Two runs the second part of the challenge
+func Two() {
+	games, err := ParseInput()
+	if err != nil {
+		slog.Error("Error parsing input: " + err.Error())
+	}
+
+	totalpower := 0
+
+	// Loop through the games
+	for _, game := range games {
+		var minBlue, minRed, minGreen int
+		// Loop through the sets
+		for _, set := range game.Sets {
+			// Loop through the results
+			for _, result := range set.Result {
+				// Looop through the cubes
+				for _, cube := range result.Cubes {
+					if cube.Color == "blue" {
+						if cube.Count > minBlue {
+							minBlue = cube.Count
+						}
+					} else if cube.Color == "red" {
+						if cube.Count > minRed {
+							minRed = cube.Count
+						}
+					}
+					if cube.Color == "green" {
+						if cube.Count > minGreen {
+							minGreen = cube.Count
+						}
+					}
+				}
+			}
+		}
+		totalpower += (minBlue * minRed * minGreen)
+	}
+	slog.Info("Solution Two: " + strconv.Itoa(totalpower))
 }
 
 func main() {
 	// Run the first part of the challenge
 	One()
+
+	// Run the second part of the challenge
+	Two()
 }
