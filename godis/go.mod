7fb16d7de76b027f79e0031631c07c07e5d7b7a3
awalvie <agrawal.vishesh.178@gmail.com>
2024-03-14 16:01:03 +0100

    godis(all): QOL changes
    * Add Makefile to the project
    * Use zerolog instead of stdlib log
    * Add a handleConnection function to handle incoming connections
    * Use go routines to handle concurrent connections

--- godis/go.mod
+++ godis/go.mod
@@ -1,3 +1,12 @@
 module github.com/awalvie/workshop/godis
 
 go 1.21.4
+
+require github.com/davecgh/go-spew v1.1.1
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/rs/zerolog v1.32.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+)
--- godis/main.go
+++ godis/main.go
@@ -1,26 +1,59 @@
 package main
 
 import (
-	"log"
 	"net"
+	"path/filepath"
+	"strconv"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 func init() {
-	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	// Configure logger
+
+	// Equivalent of Lshortfile
+	// https://github.com/rs/zerolog?tab=readme-ov-file#customize-automatic-field-names
+	zerolog.CallerMarshalFunc = func(pc uintptr, file string, line int) string {
+		return filepath.Base(file) + ":" + strconv.Itoa(line)
+	}
+	log.Logger = log.With().Caller().Logger()
 }
 
 func main() {
 	// Initialize TCP Server
-	log.Println("Starting TCP Server on localhost:6379")
-	l, err := net.Listen("tcp", "localhost:6379")
+	addr := "localhost:7000"
+	log.Info().Msgf("Starting TCP Server on %s", addr)
+	l, err := net.Listen("tcp", addr)
 	if err != nil {
-		log.Fatal("Error starting TCP Server: ", err)
+		log.Fatal().Msgf("Error starting TCP Server: %s", err)
 	}
+	defer l.Close()
 
-	// Accept incoming connection
-	_, err = l.Accept()
+	// Keep the server running forever
+	for {
+		// Accept incoming connection
+		log.Info().Msgf("waiting for incoming connection")
+
+		conn, err := l.Accept()
+		log.Info().Msgf("incoming connection from %s", conn.RemoteAddr().String())
+
+		if err != nil {
+			log.Error().Msgf("error accepting incoming connection: %s", err)
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+// handleConnection will handle the incoming TCP connection
+func handleConnection(conn net.Conn) {
+	// Read data from the connection
+	var r []byte
+	_, err := conn.Read(r)
 	if err != nil {
-		log.Fatal("Error accepting incoming connection: ", err)
+		log.Error().Msgf("error reading data from connection: %s", err)
+		return
 	}
-
+	defer conn.Close()
 }
